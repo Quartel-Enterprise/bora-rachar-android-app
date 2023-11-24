1e43e817d2dcd356e33b012bb4a25711a17ac9e6
Victor Meyer <viniciusmello201@gmail.com>
2023-11-23 23:14:13 -0300

    Interstitial setup

--- app/src/main/java/com/quare/blitzsplit/MainActivity.kt
+++ app/src/main/java/com/quare/blitzsplit/MainActivity.kt
@@ -1,23 +1,36 @@
 package com.quare.blitzsplit
 
+import android.app.Activity
+import android.content.ContentValues.TAG
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
 import com.quare.blitzsplit.ui.theme.BlitzSplitTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,7 +43,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    BannerAd("ca-app-pub-3940256099942544/6300978111") //Mock Id; será pego do AdMob
+                    Column(
+                        modifier = Modifier
+                        .fillMaxSize()
+                        .padding(8.dp),
+                        verticalArrangement = Arrangement.SpaceEvenly,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        BannerAd("ca-app-pub-3940256099942544/6300978111") //Mock Id; será pego do AdMob
+                        Button(onClick = { showInterstitialAd(this@MainActivity) }) {
+                            Text("Show interstitial ad")
+                        }
+                    }
                 }
             }
         }
@@ -39,17 +63,45 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BannerAd(adId: String) {
-    Column(modifier = Modifier.fillMaxSize()) {
-        Spacer(Modifier.height(16.dp))
-        AndroidView(
-            modifier = Modifier.fillMaxWidth(),
-            factory = { context ->
-                AdView(context).apply {
-                    setAdSize(AdSize.BANNER)
-                    adUnitId = adId
-                    loadAd(AdRequest.Builder().build())
-                }
+    AndroidView(
+        modifier = Modifier.fillMaxWidth(),
+        factory = { context ->
+            AdView(context).apply {
+                setAdSize(AdSize.BANNER)
+                adUnitId = adId
+                loadAd(AdRequest.Builder().build())
             }
-        )
+        }
+    )
+}
+fun showInterstitialAd(activity: Activity) {
+    loadInterstitialAd(activity) { interstitialAd ->
+        if (interstitialAd != null) {
+            // Ad loaded successfully, you can now show the interstitial ad
+            interstitialAd.show(activity)
+        } else {
+            // Ad loading failed or was not loaded, handle accordingly
+        }
     }
+}
+
+private fun loadInterstitialAd(activity: Activity, callback: (InterstitialAd?) -> Unit) {
+    val adRequest = AdRequest.Builder().build()
+
+    InterstitialAd.load(
+        activity,
+        "ca-app-pub-3940256099942544/1033173712",
+        adRequest,
+        object : InterstitialAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                Log.d(TAG, adError.toString())
+                callback(null)
+            }
+
+            override fun onAdLoaded(interstitialAd: InterstitialAd) {
+                Log.d(TAG, "Ad was loaded.")
+                callback(interstitialAd)
+            }
+        }
+    )
 }
