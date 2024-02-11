f79f55e356bbca33bd879194e4a02cb88a9325f7
pvieira_meli <pierre.vieira@mercadolivre.com>
2024-02-10 21:23:41 -0300

    Fix spacing in BallModel

--- core/component/src/main/java/com/quare/blitzplit/component/ball/model/BallModel.kt
+++ core/component/src/main/java/com/quare/blitzplit/component/ball/model/BallModel.kt
@@ -2,7 +2,7 @@ package com.quare.blitzplit.component.ball.model
 
 import androidx.compose.ui.graphics.Color
 
-sealed class BallModel(val color: Color ) {
+sealed class BallModel(val color: Color) {
     class Filled(color: Color) : BallModel(color)
     data class WithBorder(val borderColor: Color) : BallModel(Color.Transparent)
 }
