b421a78c2a211358694df1fbc81c6d30626b01f2
Pierre Vieira <pierrevieiraggg@gmail.com>
2024-02-11 22:45:40 -0300

    Refactor: delete google success launcher (#58)

--- feature/login/src/main/java/com/quare/blitzsplit/login/presentation/viewmodel/launcher/OnSuccessResultLauncher.kt
+++
@@ -1,7 +0,0 @@
-package com.quare.blitzsplit.login.presentation.viewmodel.launcher
-
-import androidx.activity.result.ActivityResult
-
-fun interface OnSuccessResultLauncher {
-    fun onResult(result: ActivityResult)
-}
