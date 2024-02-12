ae00d4b288851cb655997dd036ebb3850db18089
pvieira_meli <pierre.vieira@mercadolivre.com>
2024-02-12 17:02:13 -0300

    Create dialog component (util for different dialog types)

--- feature/main/src/main/java/com/quare/blitzsplit/main/presentation/component/dialog/LogoutDialog.kt
+++ feature/main/src/main/java/com/quare/blitzsplit/main/presentation/component/dialog/LogoutDialog.kt
@@ -1,55 +1,29 @@
 package com.quare.blitzsplit.main.presentation.component.dialog
 
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.quare.blitzplit.component.dialog.DialogComponent
 import com.quare.blitzsplit.theme.BlitzSplitTheme
-import com.quare.blitzsplit.theme.Blue900
 
 @Composable
 fun LogoutDialog(
     onLogout: () -> Unit,
     onDismiss: () -> Unit,
 ) {
-    AlertDialog(
-        title = {
-            Text(text = "Sair")
-        },
-        text = {
+    DialogComponent(
+        title = "Sair",
+        middleContent = {
             Text(text = "Tem certeza que deseja encerrar a sessão?")
         },
-        confirmButton = {
-            DialogTextButton(
-                text = "Confirmar",
-                action = {
-                    onDismiss()
-                    onLogout()
-                }
-            )
+        confirmButtonText = "Confirmar",
+        cancelButtonText = "Cancelar",
+        confirmButtonClick = {
+            onDismiss()
+            onLogout()
         },
-        dismissButton = {
-            DialogTextButton(
-                text = "Cancelar",
-                action = onDismiss
-            )
-        },
-        onDismissRequest = onDismiss
+        onDismiss = onDismiss
     )
-}
-
-@Composable
-private fun DialogTextButton(
-    text: String,
-    action: () -> Unit,
-) {
-    TextButton(onClick = action) {
-        Text(
-            text = text,
-            color = Blue900
-        )
-    }
 }
 
 @Composable
