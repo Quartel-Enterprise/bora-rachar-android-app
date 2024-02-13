ea29d9113dadb047bb6cef8765bdbf65ec209998
Pierre Vieira <pierrevieiraggg@gmail.com>
2024-02-13 20:12:46 -0300

    Fix: typo in payment and receive dialog (#64)

---
+++ feature/group/build.gradle.kts
@@ -0,0 +1,73 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+    id("com.google.dagger.hilt.android")
+    kotlin("kapt")
+}
+
+android {
+    namespace = "com.quare.blitzsplit.group"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
+    }
+}
+
+dependencies {
+    // Modules
+    val modules = listOf("core:component", "core:theme")
+    modules.forEach { module ->
+        implementation(project(":$module"))
+    }
+
+    // Compose
+    implementation(platform(libs.composeBom))
+    implementation(platform(libs.firebaseBom))
+    implementation(libs.ui)
+    implementation(libs.uiGraphics)
+    implementation(libs.uiToolingPreview)
+    implementation(libs.play.services.auth)
+    implementation(libs.firebase.auth.ktx)
+    implementation(libs.viewModelKtx)
+    implementation(libs.lifecycleRuntimeCompose)
+    implementation(libs.hiltNavigationCompose)
+    implementation("androidx.compose.material3:material3-android:1.2.0-rc01") /* Temporary solution for
+        loading progress indicator with material 3 (remove when it's fixed)
+        https://stackoverflow.com/a/77907893/11111289
+    */
+
+    // Hilt
+    implementation(libs.daggerHilt)
+    kapt(libs.daggerHiltCompiler)
+
+    debugImplementation(libs.uiTooling)
+    debugImplementation(libs.uiTestManifest)
+}
