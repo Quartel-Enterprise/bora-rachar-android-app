709fd3f727947b2861dd19b7da9b9388c1256d44
Pierre Vieira <pierrevieiraggg@gmail.com>
2024-03-03 23:29:17 -0300

    Show specific login error message when user refuses login too many times (#87)

--- feature/login/src/main/java/com/quare/blitzsplit/login/presentation/signin/IntentSenderProvider.kt
+++ feature/login/src/main/java/com/quare/blitzsplit/login/presentation/signin/IntentSenderProvider.kt
@@ -3,17 +3,20 @@ package com.quare.blitzsplit.login.presentation.signin
 import android.content.IntentSender
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.common.api.ApiException
 import javax.inject.Inject
 import kotlinx.coroutines.tasks.await
 
 class IntentSenderProvider @Inject constructor(
     private val signInClient: SignInClient,
 ) {
-    suspend fun signIn(): IntentSender? {
-        val result = runCatching {
-            signInClient.beginSignIn(buildSignInRequest()).await()
-        }.getOrNull()
-        return result?.pendingIntent?.intentSender
+    suspend fun signIn(): Result<IntentSender> = try {
+        val result = signInClient.beginSignIn(buildSignInRequest()).await()
+        result?.pendingIntent?.intentSender?.let { intentSender ->
+            Result.success(intentSender)
+        } ?: Result.failure(NullPointerException("IntentSender is null"))
+    } catch (apiException: ApiException) {
+        Result.failure(apiException)
     }
 
     private fun buildSignInRequest(): BeginSignInRequest = BeginSignInRequest.Builder()
